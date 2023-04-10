ab5be9918bdf1b86c264e95074ce53c879b8276e
Emeldalaeticia <emelda.owira@aiesec.net>
2023-04-10 16:50:57 +0300

    added the erroe function and prompt function

--- DivisibleNumbers.js
+++ DivisibleNumbers.js
@@ -1,11 +1,29 @@
-function sumOfNoDivisibleByThreeOrFive (n){
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 || i % 5 === 0) {
-            sum += i ;
-        } 
-    }
-    return sum;
-}
 
-console.log(sumOfNoDivisibleByThreeOrFive(45));
+function sumOfNoDivisibleByThreeOrFive() {
+    let numbers = [];
+    let sum = 0;
+    process.stdout.write("Please enter a positive integer: ");
+    process.stdin.once('data', (input) => {
+      let n = parseInt(input.toString().trim());
+      try {
+        if (!Number.isInteger(n) || n < 1) {
+          throw new Error("Invalid input. Please enter a positive integer.");
+        }
+        for (let i = 1; i <= n; i++) {
+          if (i % 3 === 0 || i % 5 === 0) {
+            numbers.push(i);
+            sum += i;
+          }
+        }
+        console.log("Numbers used:", numbers);
+        console.log("Sum:", sum);
+      } catch (err) {
+        console.error(err.message);
+      }
+      process.stdin.pause();
+    });
+    process.stdin.resume();
+  }
+  
+  sumOfNoDivisibleByThreeOrFive();
+  
